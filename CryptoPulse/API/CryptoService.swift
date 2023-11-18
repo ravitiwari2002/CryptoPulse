85d691c423d146c7d7083d760bdb1998f6f9f6bd
Ravi Tiwari <ravitiwari2002@gmail.com>
2023-11-17 22:03:32 -0700

    Sprint2

---
+++ CryptoPulse/API/CryptoService.swift
@@ -0,0 +1,32 @@
+//
+//  CryptoService.swift
+//  CryptoPulse
+//
+//  Created by Ravi Tiwari on 11/6/23.
+//
+
+import Foundation
+import Combine
+
+class CryptoService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init(){
+        getCoins()
+    }
+    
+     func getCoins(){
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        
+        coinSubscription = Networking.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: Networking.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
---
+++ CryptoPulse/API/MarketDataAPI.swift
@@ -0,0 +1,32 @@
+//
+//  MarketDataAPI.swift
+//  CryptoPulse
+//
+//  Created by Ravi Tiwari on 11/10/23.
+//
+
+import Foundation
+import Combine
+
+class MarketDataAPI {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init(){
+        getData()
+    }
+    
+     func getData(){
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = Networking.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: Networking.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
