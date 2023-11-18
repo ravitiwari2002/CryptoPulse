fd8a195b54e692e7f4f03afaed4a969f4ce68131
ravitiwari2002 <81186121+ravitiwari2002@users.noreply.github.com>
2023-11-18 00:43:07 -0700

    Update PortfolioView.swift

--- CryptoPulse/Core/Home/Views/PortfolioView.swift
+++ CryptoPulse/Core/Home/Views/PortfolioView.swift
@@ -62,7 +62,7 @@ extension PortfolioView{
     private var coinLogoList: some View{
         ScrollView(.horizontal, showsIndicators: false, content:{
             LazyHStack(spacing:10){
-                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins){ coin in
+                ForEach(vm.searchText.isEmpty ? (vm.portfolio.isEmpty ? vm.allCoins : vm.portfolio) : vm.allCoins){ coin in
                     CoinLogoView(coin:coin)
                         .frame(width: 75)
                         .padding(4)
